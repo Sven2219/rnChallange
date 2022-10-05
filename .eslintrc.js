5e9372a1990e1caacdaf1d2f5412e5fbdd9726b1
Sven <62476842+Sven2219@users.noreply.github.com>
2022-10-05 12:30:29 +0200

    Added:linter and prettier (#2)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,93 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
-  ],
+	root: true,
+	extends: [
+		'airbnb',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react', 'react-hooks', '@typescript-eslint'],
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx'],
+			rules: {
+				'@typescript-eslint/no-shadow': ['error'],
+				'no-shadow': 'off',
+				'no-undef': 'off',
+			},
+		},
+	],
+	parserOptions: {
+		project: './tsconfig.json',
+		createDefaultProgram: true,
+	},
+	rules: {
+		'react/jsx-filename-extension': [
+			2,
+			{
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			},
+		],
+		'import/extensions': 0,
+		'import/no-extraneous-dependencies': 0,
+		'no-return-await': 'off',
+		'no-empty-function': 0,
+		radix: 'off',
+		'lines-between-class-members': [
+			'error',
+			'always',
+			{
+				exceptAfterSingleLine: true,
+			},
+		],
+		'jsx-a11y/label-has-associated-control': 'off',
+		'import/prefer-default-export': 'off',
+		'no-shadow': 'off',
+		'no-unneeded-ternary': 'off',
+		'react/destructuring-assignment': 'off',
+		'import/no-mutable-exports': 'off',
+		camelcase: 'off',
+		'no-return-assign': 'off',
+		'react/jsx-props-no-spreading': 'off',
+		'react/require-default-props': 'off',
+		'no-param-reassign': 'off',
+		'jsx-a11y/click-events-have-key-events': 'off',
+		'jsx-a11y/no-noninteractive-element-interactions': 'off',
+		curly: ['error', 'all'],
+		'no-use-before-define': 'off',
+		'no-void': 'off',
+		'@typescript-eslint/no-use-before-define': ['off'],
+		'@typescript-eslint/no-explicit-any': ['error'],
+		'@typescript-eslint/no-misused-promises': [
+			'error',
+			{
+				checksVoidReturn: false,
+			},
+		],
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+		'no-useless-catch': 'off',
+		'no-useless-constructor': 'off',
+		'no-async-promise-executor': 'off',
+		'class-methods-use-this': 'off',
+		'react/prop-types': 'off',
+		'no-console': 'off',
+		'react-hooks/exhaustive-deps': 'off',
+		'func-names': 'off',
+	},
+	settings: {
+		'import/resolver': {
+			typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx', '.native.js'],
+			},
+		},
+		react: {
+			version: 'detect',
+		},
+	},
 };
